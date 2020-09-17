99b5cb1eabc9be11d144cb883a4121a84e1dfbdf
nickypangers <nixonpang98@gmail.com>
2020-09-17 17:09:48 +0800

    hello world

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <p>Hello World</p>
+    </div>
+  );
+}
+
+export default Header;

8164a6f8d760a94b0b245466c997e26b733e6a79
nickypangers <nixonpang98@gmail.com>
2020-09-18 12:29:15 +0800

    Updates

---
+++ precache-manifest.48a6ec8820d873d67324d105da29fd00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71bbc9586c9bdf657c6689936c8e723d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97958088b5413c43b3a1",
+    "url": "/static/css/main.3c0769b4.chunk.css"
+  },
+  {
+    "revision": "848e6021136e541b7e7e",
+    "url": "/static/js/2.d5d18a0e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.d5d18a0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97958088b5413c43b3a1",
+    "url": "/static/js/main.25855e18.chunk.js"
+  },
+  {
+    "revision": "80bbb3f6ff05d5261a4b",
+    "url": "/static/js/runtime-main.2f41999c.js"
+  }
+]);
